c145991221064583c6fe78b46bfecfbfe25d4030
mharris <throwingfish@gmail.com>
2026-02-01 10:58:12 -0500

    updating comment

--- src/renderer/Components/Clock/Clock.tsx
+++ src/renderer/Components/Clock/Clock.tsx
@@ -1,5 +1,18 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Components and Hooks must be pure
+ *  - Components with side effects should run separetely from rendering
+ *  - Has no side effects in render
+ *  - Does not mutate non-local values (outside scope))
+ * From:
+ *  https://react.dev/reference/rules/components-and-hooks-must-be-pure#components-and-hooks-must-be-idempotent
+ * Purity: 
+ *  https://mostly-adequate.gitbook.io/mostly-adequate-guide/ch03
+ * See
+ *  https://react.dev/learn/keeping-components-pure#side-effects-unintended-consequences
+ *  https://react.dev/learn/keeping-components-pure#side-effects-unintended-consequences
+ */
 function useTime(): Date {
     const [time, setTime] = useState(() => new Date());
     useEffect(() => {
